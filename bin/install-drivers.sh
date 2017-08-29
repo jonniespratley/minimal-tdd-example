bd54ac10a72c55871931fb9bf1bd839f8da8b0c9
Jonnie Spratley <jonnie.spratley@ge.com>
2017-08-29 13:46:30 -0700

    updated install config

--- bin/install-drivers.sh
+++ bin/install-drivers.sh
@@ -5,14 +5,16 @@ set -e
 SELENIUM_VERSION=3.0.1
 CHROMEDRIVER_VERSION=2.31
 
-echo "Downloading Selenium Standalone Server $SELENIUM_VERSION"
 
+cd ./bin
+
+echo "Downloading Selenium Standalone Server $SELENIUM_VERSION"
 curl -o ./selenium-server-standalone-$SELENIUM_VERSION.jar \
 	https://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-$SELENIUM_VERSION.jar
 
-echo "Downloading Chromedriver"
+echo "Downloading chromedriver $CHROMEDRIVER_VERSION"
 curl -o ./chromedriver.zip \
 	https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_mac64.zip
 
-echo "Unzipping chromedirver"
+echo "Unzipping chromedriver $CHROMEDRIVER_VERSION"
 unzip ./chromedriver.zip -d ./bin
--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,30 +2,28 @@
 const path = require('path');
 
 //http://nightwatchjs.org/gettingstarted#basic-settings
-const config = {
-  "src_folders" : ["test/integration/specs"],
-  "output_folder" : "reports",
-  "page_objects_path" : "test/integration/pages",
-  "globals_path" : "",
-  "selenium" : {
-    "start_process" : true,
-    "server_path" : path.resolve(__dirname, './bin/selenium-server-standalone-3.0.1.jar'),
-    "port" : 4444,
-    "cli_args" : {
-      "webdriver.chrome.driver" : path.resolve(__dirname, './bin/chromedriver')
-    }
-  },
+module.exports = {
+	"src_folders": ["test/integration/specs"],
+	"output_folder": "reports",
+	"page_objects_path": "test/integration/pages",
+	"globals_path": "",
+	"selenium": {
+		"start_process": true,
+		"server_path": path.resolve(__dirname, './bin/selenium-server-standalone-3.0.1.jar'),
+		"port": 4444,
+		"cli_args": {
+			"webdriver.chrome.driver": path.resolve(__dirname, './bin/chromedriver')
+		}
+	},
 
-  "test_settings" : {
-    "default" : {
-      "launch_url" : "http://localhost:9001",
-      "selenium_port"  : 4444,
-      "selenium_host"  : "localhost",
-      "desiredCapabilities": {
-        "browserName": "chrome"
-      }
-    }
-  }
+	"test_settings": {
+		"default": {
+			"launch_url": "http://localhost:9001",
+			"selenium_port": 4444,
+			"selenium_host": "localhost",
+			"desiredCapabilities": {
+				"browserName": "chrome"
+			}
+		}
+	}
 };
-
-module.exports = config;
